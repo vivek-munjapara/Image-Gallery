6af0952c6a04e54a1da6ff3d362411958e60e5e0
Vivek <vivek.munjapara@gmail.com>
2023-01-07 17:42:40 +0530

    image gallery done

---
+++ js/main.js
@@ -0,0 +1,65 @@
+// let perPage = perPage + 10;
+const inputValue = document.getElementById('input');
+
+let perPage = 10;
+
+console.log(perPage);
+let myArray = [];
+
+async function getValue(inputValue, perPage) {
+
+    document.getElementById('error').innerHTML = `<h2 class="text-light">Loading...</h2>`
+
+
+    let url = await fetch(`https://api.pexels.com/v1/search?query=${inputValue}&per_page=${perPage}`,
+        {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                AUthorization: '563492ad6f91700001000001a50248a19756492687753a2523d04a50'
+            }
+        })
+
+    let urldata = await url.json();
+
+    if (urldata.status != 400 && urldata.status != 404 && inputValue.lenght != 0) {
+        display(urldata);
+        document.getElementById('error').innerHTML = ``
+    } else {
+        document.getElementById("error").innerHTML = `<h2 class="text-light">Opp! input field can't be empty</h2>`;
+        document.querySelector("#hello").style.display = "none";
+
+    }
+}
+
+
+document.querySelector("form").addEventListener("submit", (event) => {
+    getValue(inputValue.value, perPage);
+    event.preventDefault();
+
+})
+
+
+loadmore = () => {
+
+    perPage += 10;
+    // console.log(perPage);
+    getValue(inputValue.value, perPage);
+
+}
+
+
+display = (data) => {
+
+    myArray = data.photos;
+
+    document.getElementById('photo').innerHTML = myArray.map((value) => {
+        return `
+            <div class="photodiv picmrgn divheight">
+                <img src="${value.src.original}" class="img-fluid" alt="...">
+            </div>`
+    }).join("");
+    document.querySelector("#ldmr-btn").style.display = "block";
+
+    console.log(myArray);
+}
